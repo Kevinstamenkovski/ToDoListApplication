70a6dc33e7a4a2edb5db6202737772774d398f24
Kevin_stamenkovski <63673430+Kevinstamenkovski@users.noreply.github.com>
2024-02-25 21:04:14 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/todolistapplication/MainActivity.java
+++ app/src/main/java/com/example/todolistapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.todolistapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
